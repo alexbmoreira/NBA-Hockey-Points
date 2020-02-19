e736ab7976a174489cee4f0c4ae0e47baf77f432
Alex Moreira <alexbmoreira@gmail.com>
2020-02-19 13:58:46 -0500

    Updated plotter.py

--- plotter.py
+++ plotter.py
@@ -6,12 +6,15 @@ def get_data():
     my_data = pd.read_csv("hockey_points.csv", delimiter=",")
 
     #print(my_data)
-    print(my_data["team name"])
+    #print(my_data["team name"])
+    #print(my_data.loc["0":, "points"])
+
+    print(my_data["points"]["93"])
 
     #fig = plt.figure()
     #axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
     #axes.plot(my_data["points"], my_data["team_name"])
-    #plt.show()
+    plt.show()
 
 get_data()

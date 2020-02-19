5636c60000d1b3d7e55dbc33dbb2932dd6d33f88
Alex Moreira <alexbmoreira@gmail.com>
2020-02-19 14:11:29 -0500

    Updated plotter.py

--- plotter.py
+++ plotter.py
@@ -3,18 +3,15 @@ import pandas as pd
 import scrape
 
 def get_data():
-    my_data = pd.read_csv("hockey_points.csv", delimiter=",")
+    my_data = pd.read_csv("hockey_points.csv")
 
     #print(my_data)
     #print(my_data["team name"])
-    print(my_data.loc["0":, ["team name", "points"]])
-
-    #print(type(my_data["points"].loc["0"]))
+    print(my_data.loc["0":, ["team name", "points"]].dtypes)
 
     #fig = plt.figure()
     #axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
     #axes.plot(my_data["points"], my_data["team_name"])
-    plt.show()
 
 get_data()

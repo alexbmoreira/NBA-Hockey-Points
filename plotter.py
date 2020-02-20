e9fb5d8c2c5b6e0e32a09d5038de75dfd27fb87c
Alex Moreira <alexbmoreira@gmail.com>
2020-02-19 23:15:56 -0500

    Updated plotter.py

--- plotter.py
+++ plotter.py
@@ -4,6 +4,26 @@ import pandas as pd
 import scrape
 
 def get_data():
-    my_data = np.read_csv("hockey_points.csv", index_col=0)
+    my_data = np.genfromtxt("hockey_points.csv", delimiter=",", skip_header=1,\
+                            dtype=[('rank', '<i4'), ('team', '<U3'), ('gp', '<i4'), \
+                            ('w', '<i4'), ('l', '<i4'), ('otl', '<i4'), ('p', '<i4'), ('rw', '<i4'), ])
+
+
+    y = my_data["p"]
+    x = my_data["team"]
+
+    fig = plt.figure()
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+    axes.barh(x, y)
+
+    axes.set_xlabel("Points")
+    axes.set_ylabel("Team")
+    axes.set_title("NBA + NHL Combined Points Standings")
+
+    plt.hlines(y=y, xmin=0, xmax=61, color='#007acc', alpha=0.2, linewidth=5)
+    plt.gca().invert_yaxis()
+    plt.show()
+    #print(my_data)
 
 get_data()

4b1f19827da48ab62347ca74e45b41be02a4b585
Alex Moreira <alexbmoreira@gmail.com>
2020-02-20 20:02:29 -0500

    Updated plotter.py

--- plotter.py
+++ plotter.py
@@ -3,10 +3,9 @@ import numpy as np
 import pandas as pd
 import scrape
 
-def get_data_pd():
+def graph_data():
     my_data = pd.read_csv("hockey_points.csv", index_col="rank")
-    standings = my_data.loc["1":, ["points", "team name"]]
-
+    my_data = my_data.sort_values("rank", ascending=False)
     my_range = list(range(1, len(my_data.index) + 1))
 
     fig, ax = plt.subplots(figsize=(4.5,4.5))
@@ -26,35 +25,9 @@ def get_data_pd():
     ax.spines['left'].set_smart_bounds(True)
     ax.spines['bottom'].set_smart_bounds(True)
 
-    #print(my_data)
-    #print(standings)
-    plt.gca().invert_yaxis()
     plt.tight_layout()
     plt.savefig('standings.png', dpi=300, bbox_inches='tight')
     plt.show()
 
-def get_data_np():
-    my_data = np.genfromtxt("hockey_points.csv", delimiter=",", skip_header=1,\
-                            dtype=[('rank', '<i4'), ('team', '<U3'), ('gp', '<i4'), \
-                            ('w', '<i4'), ('l', '<i4'), ('otl', '<i4'), ('p', '<i4'), ('rw', '<i4'), ])
-
-
-    y = my_data["p"]
-    x = my_data["team"]
-
-    fig = plt.figure()
-    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-
-    axes.barh(x, y)
-
-    axes.set_xlabel("Points")
-    axes.set_ylabel("Team")
-    axes.set_title("NBA + NHL Combined Points Standings")
-
-    plt.hlines(y=y, xmin=0, xmax=61, color='#007acc', alpha=0.2, linewidth=5)
-    plt.gca().invert_yaxis()
-    plt.show()
-    #print(my_data)
-
-#get_data_np()
-get_data_pd()
+    
+graph_data()

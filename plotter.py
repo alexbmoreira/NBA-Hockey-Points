e52541cd8ecb5bf0f95b34ded0f7c855ac368289
Alex Moreira <alexbmoreira@gmail.com>
2020-02-22 01:30:07 -0500

    Updated plotter.py

--- plotter.py
+++ plotter.py
@@ -8,26 +8,22 @@ import scrape
 def graph_data():
     my_data = pd.read_csv("hockey_points.csv", index_col="rank").sort_values("rank", ascending=False)
     ppg_data = my_data.sort_values("ppg", ascending=True)
-    nhl_ppg = ppg_data["league"] == "NHL"
-    nba_ppg = ppg_data["league"] == "NBA"
+    #nhl_ppg = ppg_data["league"] == "NHL"
+    #nba_ppg = ppg_data["league"] == "NBA"
     my_range = list(range(1, len(my_data.index) + 1))
 
-    fig, ((ax_st, ax_ppg), (std_nba, std_nhl)) = plt.subplots(nrows=2, ncols=2, figsize=(9,7.5), gridspec_kw={'height_ratios': [3, 1]})
-
-
+    fig, ax_st = plt.subplots(figsize=(5, 5))
+    fig, ax_ppg = plt.subplots(figsize=(5, 5))
 
     axes_st(ax_st, my_data, my_range)
-
     axes_ppg(ax_ppg, ppg_data, my_range)
 
-    plt.tight_layout()
-    plt.savefig('standings.png', dpi=300, bbox_inches='tight')
     plt.show()
 
 def axes_ppg(axes, data, my_range):
     axes.set_xlabel("Points per Game", fontsize=9, fontweight='black', color = '#333F4B')
     axes.set_ylabel("Team", fontsize=9, fontweight='black', color = '#333F4B')
-    axes.set_title("Sorted by Points per Game", fontsize=11, fontweight='black', color = '#333F4B')
+    axes.set_title("NBA + NHL Standings (by PPG)", fontsize=11, fontweight='black', color = '#333F4B')
     axes.spines['top'].set_color('none')
     axes.spines['right'].set_color('none')
     axes.spines['left'].set_smart_bounds(True)
@@ -39,11 +35,13 @@ def axes_ppg(axes, data, my_range):
     plt.sca(axes)
     plt.yticks(my_range, data["team name"], fontsize=3)
     plt.xticks(list(np.linspace(0,1.75,20)), fontsize=5, rotation=45)
+    plt.tight_layout()
+    plt.savefig('standings_points.png', dpi=300, bbox_inches='tight')
 
 def axes_st(axes, data, my_range):
     axes.set_xlabel("Points", fontsize=9, fontweight='black', color = '#333F4B')
     axes.set_ylabel("Team", fontsize=9, fontweight='black', color = '#333F4B')
-    axes.set_title("Sorted by Points", fontsize=11, fontweight='black', color = '#333F4B')
+    axes.set_title("NBA + NHL Standings (by points)", fontsize=11, fontweight='black', color = '#333F4B')
     axes.spines['top'].set_color('none')
     axes.spines['right'].set_color('none')
     axes.spines['left'].set_smart_bounds(True)
@@ -55,5 +53,7 @@ def axes_st(axes, data, my_range):
     plt.sca(axes)
     plt.yticks(my_range, data["team name"], fontsize=3)
     plt.xticks(list(range(0, 101, 5)), fontsize=5)
+    plt.tight_layout()
+    plt.savefig('standings_ppg.png', dpi=300, bbox_inches='tight')
     
 graph_data()
